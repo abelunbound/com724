76f09e5a5a489298684134c8a4bb69f90c81db87
Abel Akeni <5akena74@solent.ac.uk>
2022-07-26 15:30:36 +0100

    Update

---
+++ sentiment_analysis/keys.py
@@ -0,0 +1,7 @@
+
+consumer_key="rlkHtI4o4qLGLH3UAieiIeKHP"
+consumer_secret="Zrfpj8JQDJAd0auZTlaq9Am5NDaRhrQWxptU4sgSvomfwJdtbx"
+access_token="255262374-0vSw2ulF5xZ0fFP9cmRe26SDUv6jBMBPXFrGbTfy"
+access_token_secret="6FOtuJjV0Ta6PzFYQF2u0hJrXxQciP80zSxT54uNFmfSP"
+bearer_token="AAAAAAAAAAAAAAAAAAAAALSKfAEAAAAAOmrHrtmQZ%2Bt%2FULaYcZ1Iq%2FL4QY8%3D9ByTmeXrXWFDCw0eimRteV2iIQrieDVAhmQHPnIHLpquSsdp7C"
+
