816d1a2ff0b83149798c1e1781241f8dc64e1dc5
Abel Akeni <5akena74@solent.ac.uk>
2022-07-30 09:54:29 +0100

    upadte

--- assessment/gui.py
+++ assessment/gui.py
@@ -9,25 +9,15 @@ app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Test hot reload Analytics'),
-
-    html.Div(children=
-    '''
-        Dash: A web application framework for your data.
-    '''),
-
+app.layout = html.Div([
     dcc.Graph(
-        id='example-graph',
+        id='life-exp-vs-gdp',
         figure=fig
     )
 ])
---
+++ gui_dash_backup/README.md
@@ -0,0 +1,8 @@
+# dash_template
+Python Dash data visualization template
+
+
+This is a sample Python Dash template which can be used to create data visualization board.
+
+![alt text](https://github.com/ishanmehta17/dash_template/blob/master/dash_template_web_img.jpg)
+
